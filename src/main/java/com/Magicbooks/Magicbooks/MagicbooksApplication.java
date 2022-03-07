a094426fff7e1ac5c521de37118f0a7b1b7660c5
Swapnil Jawalekar <Swapnil.Jawalekar@communitybrands.com>
2022-03-07 11:45:22 +0530

    first commit

---
+++ src/main/java/com/Magicbooks/Magicbooks/MagicbooksApplication.java
@@ -0,0 +1,13 @@
+package com.Magicbooks.Magicbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Magicbooks/Magicbooks/MagicbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Magicbooks.Magicbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
